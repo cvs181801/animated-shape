01f0039bdbb0d5b6210948bd3d4ccbf0a797e66e
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-08-09 18:51:07 -0700

    add basic logic to e listener

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ document.addEventListener('click', function(e) {
     e.preventDefault();
     const x = e.pageX;
     const y = e.pageY;
-    cube.style.left = (x + .5) + "px";
+    console.log(x);
+    cube.style.left = (x - 150) + "px";
     
 })
