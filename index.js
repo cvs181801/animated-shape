563ecc36598c09e6dc3d23df3b4c5d4228d1ef13
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-08-11 09:53:04 -0700

    try it with the rotateX and rotateY formula

--- index.js
+++ index.js
@@ -25,37 +25,19 @@ function rotate() {
 }
 
 
-// let currentClass="";
-
-// function rollDice() {
-//     let randomNumber = randomNum();
-//     console.log(randomNumber);
-//     let showClass = "show__" + randomNumber;
-//     console.log(showClass);
-//     if (currentClass) {
-//         cube.classList.remove(currentClass);
-//     }
-//         cube.classList.add(showClass);
-//         currentClass = showClass;
-// }
-
-// console.log(rollDice());
-
 document.addEventListener('mouseover', function(e) {
     e.preventDefault();
-    //const x = e.pageX;
-    //const y = e.pageY;
-    //console.log(x);
-    //console.log(y);
-    //const midY = y - window.innerHeight / 2;
-    //const midX = x - window.innerWidth / 2;
-    //console.log(midY);
-    //console.log(window.innerHeight);
-    //console.log(window.innerWidth);
+    const x = e.pageX;
+    const y = e.pageY;
+    const midY = y - window.innerHeight / 2;
+    const midX = x - window.innerWidth / 2;
     //cube.style.left = x + "px";
     //cube.style.top = y + "px";
-    return rotate();
-    //cube.style.transform = "rotateX(" + (midY * 0.5) + "deg) rotateY(" + midX + "deg)";
+    cube.style.transform = "rotateX(" + (midY * 0.5) + "deg) rotateY(" + midX + "deg)";
+    //cube.style.transition = "transform ease 1s";
+    
+    //there are 2 options here, one is to use the random number generator to make it move, the other is to use the above line using the x and y values 
 
+    //return rotate();
     
 })
