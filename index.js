ab463318f7ad6c8ad16b0ec9cf23a192cfdfcf89
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-08-10 18:33:45 -0700

    change it to mousemove listener

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 
 const cube = document.querySelector(".cube__container");
 
-document.addEventListener('click', function(e) {
+document.addEventListener('mousemove', function(e) {
     e.preventDefault();
     const x = e.pageX;
     const y = e.pageY;
     console.log(x);
-    cube.style.left = (x - 150) + "px";
+    cube.style.left = x + "px";
+    cube.style.top = y + "px";
     
 })
