dbd20a3780c10412cf99f1a27ecd6f00d510903f
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-08-10 19:06:29 -0700

    try adding the show class class method, then back to the mid x and mid y method

--- index.js
+++ index.js
@@ -2,12 +2,58 @@
 
 const cube = document.querySelector(".cube__container");
 
+
+function randomNum() {
+    const result = Math.floor(Math.random()* 6);
+    if (result >= 1) {
+        return result;
+    } else {
+        return 6;
+    }
+}
+
+currentClass = "";
+
+function rotate() {
+    randomNumber = randomNum();
+    let showClass = "show__" + randomNumber;
+    if (currentClass) {
+        cube.classList.remove(currentClass);
+    } 
+    cube.classList.add(showClass);
+    currentClass = showClass;
+}
+
+console.log(rotate());
+// let currentClass="";
+
+// function rollDice() {
+//     let randomNumber = randomNum();
+//     console.log(randomNumber);
+//     let showClass = "show__" + randomNumber;
+//     console.log(showClass);
+//     if (currentClass) {
+//         cube.classList.remove(currentClass);
+//     }
+//         cube.classList.add(showClass);
+//         currentClass = showClass;
+// }
+
+// console.log(rollDice());
+
 document.addEventListener('mousemove', function(e) {
     e.preventDefault();
     const x = e.pageX;
     const y = e.pageY;
-    console.log(x);
+   // console.log(x);
+    //console.log(y);
+    const midY = y - window.innerHeight / 2;
+    const midX = x - window.innerWidth / 2;
+    //console.log(midY);
+    //console.log(window.innerHeight);
     cube.style.left = x + "px";
     cube.style.top = y + "px";
+    //return rotate();
+    cube.style.transform = "rotateX(" + (midY * 0.5) + "deg) rotateY(" + midX + "deg)";
     
 })
